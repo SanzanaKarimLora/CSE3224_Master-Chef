4d5f8125f27a51316ca143291baaea86f5215cda
SanzanaKarimLora <lorasanzana@gmail.com>
2019-04-16 16:15:21 +0600

    advanced search

--- Master_Chef/Master_Chef/Default.aspx
+++ Master_Chef/Master_Chef/Default.aspx
@@ -19,6 +19,46 @@
     <!-- Core Stylesheet -->
     <link rel="stylesheet" href="style.css">
 
+     <style type="text/css">
+        body
+        {
+            font-family:'Comic Sans MS';
+            font-size:20pt;
+        }
+
+        input[type=text]
+        {
+            margin-bottom:10pt;
+        }
+
+        .butn {
+  background: #3498db;
+  background-image: -webkit-linear-gradient(top, #3498db, #2980b9);
+  background-image: -moz-linear-gradient(top, #3498db, #2980b9);
+  background-image: -ms-linear-gradient(top, #3498db, #2980b9);
+  background-image: -o-linear-gradient(top, #3498db, #2980b9);
+  background-image: linear-gradient(to bottom, #3498db, #2980b9);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 20px;
+  padding: 10px 20px 10px 20px;
+  text-decoration: none;
+}
+
+.butn:hover {
+  background: #3cb0fd;
+  background-image: -webkit-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -moz-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -ms-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -o-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
+  text-decoration: none;
+}
+    </style>
+
 </head>
 
 <body>
@@ -78,6 +118,16 @@
                 </div>
             </div>
         </div>
+
+        <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Enter ingredients to search recipe"></asp:Label>
+        <asp:Panel ID="pnlSearch" runat="server" CssClass="divdata" BackColor="#33cc33"></asp:Panel>
+        <div>
+            <asp:Button ID="btnAddIn" runat="server" Text="Add ingredients" CssClass="butn" OnClick="btnAddIn_Click" />
+            <asp:Button ID="btngo" runat="server" Text="Go" CssClass="butn" OnClick="btngo_Click" />
+        </div>
+        <asp:Label ID="lblPrep" runat="server" Text=""></asp:Label>
+    </form>
 
         <!-- Navbar Area -->
         <div class="delicious-main-menu">
--- Master_Chef/Master_Chef/Default.aspx.cs
+++ Master_Chef/Master_Chef/Default.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +9,87 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void Page_PreInit(object sender, EventArgs e)
     {
+        List<string> keys = Request.Form.AllKeys.Where(key => key.Contains("txtDynamic")).ToList();
+        int i = 1;
+        foreach (string key in keys)
+        {
+            this.createBox("txtDynamic" + i);
+            i++;
+        }
+    }
 
+    protected void btnAddIn_Click(object sender, EventArgs e)
+    {
+        int index = pnlSearch.Controls.OfType<TextBox>().ToList().Count + 1;
+        this.createBox("txtDynamic" + index);
+
+    }
+
+    private void createBox(string id)
+    {
+        TextBox txt = new TextBox();
+        txt.ID = id;
+        pnlSearch.Controls.Add(txt);
+        Literal lt = new Literal();
+        lt.Text = "<br />";
+        pnlSearch.Controls.Add(lt);
+        
+    }
+    protected void btngo_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string qu = "";
+            foreach (TextBox textBox in pnlSearch.Controls.OfType<TextBox>())
+            {
+                List<string> li = new List<string>();
+                // string[] s = new string[10];
+                /*foreach (char s in Convert.ToString(textBox.Text))
+                {
+
+                    Label1.Text = Convert.ToString(s);
+                }*/
+                li.Add(textBox.Text);
+                int size = li.Count;
+                //for(int i=0; i<size;i++)
+                //Label1.Text += li[i]+",";
+
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString());
+                con.Open();
+
+                // string queryPrep = "select recipe_title from Recipe where recipe_id in (select recipe_id from Recipe_Ingredients where i_id in (select i_id from Ingredients where i_name = ";
+
+                for (int i = 0; i < size; i++)
+                {
+                    // queryPrep  += li[i];
+                    string q = " or";
+                    qu = li[i];
+                    //qu += q;
+
+                }
+
+                string queryPrep = "select recipe_title from Recipe where recipe_id in (select recipe_id from Recipe_Ingredients where i_id in (select i_id from Ingredients where i_name ='" + qu + "'))";
+                SqlCommand cmdprep = new SqlCommand(queryPrep, con);
+
+                string outputpt = cmdprep.ExecuteScalar().ToString();
+
+                con.Close();
+
+                if (!string.IsNullOrEmpty(outputpt))
+                    lblPrep.Text = outputpt;
+                else
+                    lblPrep.Text = "Not found";
+
+
+
+
+            }
+        }
+        catch (Exception ex)
+        {
+            lblPrep.Text = "No item found!!!";
+        }
     }
 }
